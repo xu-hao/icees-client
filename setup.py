bf69be9077455c2491271356f1e9163b6179dafd
Hao Xu <xuh@cs.unc.edu>
2019-01-23 17:56:27 -0500

    update

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="icees-client",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Colin Curtis, Hao Xu",
+    author_email="ckcurtis@renci.org, xuhao@renci.org",
+    description="ICEES client",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/xu-hao/icees-client",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
